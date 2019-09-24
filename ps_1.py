7d0ed3768263887709a1775e87e2405ca4a855b0
Kevin Zhang <zkx0804@gmail.com>
2019-09-23 20:45:42 -0400

    Add file

--- ps_1.py
+++ ps_1.py
@@ -1,27 +1,57 @@
+from typing import List
+import numpy as np
+
+
 # problem set 1
+# Sudoku
+class Problem:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        if not board:
+            return
+        self.solve(board)
 
+    def solve(self, board: List[List[str]]) -> None:
+        nums_str = "123456789"
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    for c in nums_str:
+                        if self.is_valid(board, i, j, c):
+                            board[i][j] = c
 
-class Problem1:
-    p_name = "Campus bikes. LC1057/1066"
-    p_description = {'algorithm': "Hungarian Maximum Matching Algorithm (KM, O(n^3))",
-                     'links': ["https://brilliant.org/wiki/hungarian-matching/",
-                               "https://blog.csdn.net/u011721440/article/details/38169201"]
-                     }
-    input_args = None
+                            if self.solve(board):
+                                return True
+                            else:
+                                board[i][j] = '.'
+                    return False
 
-    def __init__(self):
-        pass
+        return True
 
-    def solve(self, input_args):
-        if not input_args and not self.input_args:
-            print("No args found.")
-            return None
+    def is_valid(self, board, row, col, c):
+        for i in range(9):
+            # Check columns
+            if board[i][col] != '.' and board[i][col] == c:
+                return False
+            # Check rows
+            if board[row][i] != '.' and board[row][i] == c:
+                return False
 
-        print("Input: \r%s" % self.input_args)
-
-        print("Done!")
+            # Check 3x3 square
+            if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] != '.' and board[3 * (row // 3) + i // 3][
+                3 * (col // 3) + i % 3] == c:
+                return False
+        return True
 
 
 if __name__ == "__main__":
-    # Run actual funcs
-    print('Run!')
+    # Run actual funcs666666666
+    test = [["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+             [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+             ["4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+             [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+             [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+    solver = Problem()
+
+    solver.solveSudoku(test)
+
+    print(test)
